78702fb7c969a3cf03213cb97069c239e3d45047
JavaStudentAlex <tarasov.oleksander@gmail.com>
2025-04-10 18:00:56 +0200

    Add the lightning module for training.

--- src/optimizers/torch_optimizer.py
+++ src/optimizers/torch_optimizer.py
@@ -1,24 +1,25 @@
 from ..data.simulation import Simulation, SimulationData, CoilConfig
 from skimage.measure import label, regionprops
 
-from einops import rearrange, repeat, einsum
-import numpy as np
-import torch
-import tqdm
-import time
-
 from tqdm import trange
+import torch
+import lightning.pytorch as pl
+from einops import repeat, einsum
+from skimage.measure import label, regionprops
+from torch.utils.data import DataLoader, Dataset
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+from src.data.simulation import Simulation
+from src.costs.torch_cost_functions import TorchB1HomogeneityCostSAR
 
 
-class PhaseShift(torch.nn.Module):
+class Module(pl.LightningModule):
     def __init__(self):
-        super().__init__()
+        super(Module, self).__init__()
         self.phase = torch.nn.Parameter(torch.randn(8, dtype=torch.float32))
         self.amplitude = torch.nn.Parameter(torch.randn(8, dtype=torch.float32))
+        self.loss_fn = TorchB1HomogeneityCostSAR()
 
-    def forward(self, field):
+    def _shift_phase(self, field):
         re_phase = torch.cos(self.phase) * self.amplitude
         im_phase = torch.sin(self.phase) * self.amplitude
         coeffs_real = torch.stack((re_phase, -im_phase), dim=0)
@@ -31,6 +32,53 @@ class PhaseShift(torch.nn.Module):
             "hf reim fieldxyz ... coils, hf reimout reim coils -> hf reimout fieldxyz ...",
         )
         return field_shift
+    
+    def _field_and_prop(self, field, properties, mask):
+        labeled_mask = label(mask)
+        regions = regionprops(labeled_mask)
+        coords = regions[0].bbox
+        field_np = field[
+            :,
+            :,
+            :,
+            int(coords[0]) : int(coords[3]),
+            int(coords[1]) : int(coords[4]),
+            int(coords[2]) : int(coords[5]),
+            :,
+        ]
+        prop_np = properties[
+            :,
+            int(coords[0]) : int(coords[3]),
+            int(coords[1]) : int(coords[4]),
+            int(coords[2]) : int(coords[5]),
+        ]
+
+        field = torch.tensor(field_np, dtype=torch.float32)
+        prop = torch.tensor(prop_np, dtype=torch.float32)
+
+        return field, prop
+    
+    def configure_optimizers(self):
+        return torch.optim.AdamW(self.parameters(), lr=1e-3)
+    
+    def training_step(self, batch, batch_idx):
+        field, properties, mask = batch["field"][0], batch["properties"][0], batch["mask"][0]
+        field, properties = self._field_and_prop(field, properties, mask)
+        field_shift = self._shift_phase(field)
+        loss = self.loss_fn.calc_loss(field_shift, properties)
+        self.log("loss", loss, prog_bar=True)
+        return loss
+    
+
+class SingleSampleDataset(Dataset):
+    def __init__(self, data_dict):
+        self.data = data_dict
+    
+    def __len__(self):
+        return 1  # Just one sample
+    
+    def __getitem__(self, idx):
+        return self.data
 
 
 class TorchOptimizer:
@@ -39,78 +87,39 @@ class TorchOptimizer:
         self.cost_fuction = cost_function
         self.max_iter = max_iter
 
-    def field_and_prop(self, sim_data: Simulation):
-        labeled_mask = label(sim_data.simulation_raw_data.subject)
-        regions = regionprops(labeled_mask)
-        coords = regions[0].bbox
-        field_np = sim_data.simulation_raw_data.field[
-            :,
-            :,
-            :,
-            int(coords[0]) : int(coords[3]),
-            int(coords[1]) : int(coords[4]),
-            int(coords[2]) : int(coords[5]),
-            :,
-        ]
-        prop_np = sim_data.simulation_raw_data.properties[
-            :,
-            int(coords[0]) : int(coords[3]),
-            int(coords[1]) : int(coords[4]),
-            int(coords[2]) : int(coords[5]),
-        ]
-
-        field = torch.tensor(field_np, dtype=torch.float32).to(device)
-        prop = torch.tensor(prop_np, dtype=torch.float32).to(device)
-
-        return field, prop
-
     def optimize(self, simulation: Simulation):
-        field, prop = self.field_and_prop(simulation)
-        model = PhaseShift().to(device)
-        optmizer = torch.optim.AdamW(model.parameters(), lr=0.1)
+        data = {
+            "field": simulation.simulation_raw_data.field,
+            "properties": simulation.simulation_raw_data.properties,
+            "mask": simulation.simulation_raw_data.subject,
+        }
 
-        # Early stopping parameters
-        patience = 100  # Number of iterations to wait for improvement
-        best_loss = float("inf")
-        patience_counter = 0
+        dataset = SingleSampleDataset(data)
+        dataloader = DataLoader(dataset, batch_size=1)
 
-        # Timer to ensure the process fits within 5 minutes
-        start_time = time.time()
-        max_time = 5 * 58  # 5 minutes in seconds
+        checkpoint_callback = pl.callbacks.ModelCheckpoint(
+            monitor="loss",    # Metric you want to monitor
+            mode="min",            # 'min' because lower validation loss is better
+            save_top_k=1,          # Save only the best checkpoint
+            dirpath="checkpoints", # Directory to store checkpoints
+            filename="best_checkpoint"  # Static name; you can also use dynamic formatting.
+        )
 
-        pbar = trange(self.max_iter, desc="Optimizing", leave=True)
-        for i in pbar:
-            optmizer.zero_grad()
-            shifted_field = model(field)
-            loss = self.cost_fuction.calc_loss(shifted_field, prop)
-            loss.backward()
-            optmizer.step()
+        trainer = pl.Trainer(
+            max_epochs=5000,
+            max_time={"minutes": 4},
+            accelerator="cpu",
+            callbacks=[checkpoint_callback]
+        )
 
-            # Check for improvement
-            if loss.item() < best_loss:
-                best_loss = loss.item()
-                patience_counter = 0  # Reset patience counter
-            else:
-                patience_counter += 1
+        module = Module()
 
-            # Stop if patience is exceeded
-            if patience_counter >= patience:
-                print(f"Early stopping at iteration {i}: Loss = {loss.item()}")
-                break
+        trainer.fit(module, dataloader)
 
-            # Stop if time exceeds 5 minutes
-            elapsed_time = time.time() - start_time
-            if elapsed_time > max_time:
-                print(
-                    f"Stopping due to time limit at iteration {i}: Loss = {loss.item()}"
-                )
-                break
-
-            # Update tqdm with the current loss
-            pbar.set_postfix({"loss": loss.item(), "best_loss": best_loss})
+        best_model = Module.load_from_checkpoint(checkpoint_callback.best_model_path)
 
         coil_config = CoilConfig(
-            phase=model.phase.cpu().detach().numpy(),
-            amplitude=model.amplitude.cpu().detach().numpy(),
+            phase=best_model.phase.cpu().detach().numpy(),
+            amplitude=best_model.amplitude.cpu().detach().numpy(),
         )
         return coil_config
