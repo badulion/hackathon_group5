bf6eb32a67702070cb0ba3e25b868d76a971600c
alzbetaH <a.hrabosova@gmail.com>
2025-04-10 15:45:00 +0200

    Torch optimizer

---
+++ src/costs/torch_cost_functions.py
@@ -0,0 +1,50 @@
+from .base import BaseCost
+from ..data.simulation import SimulationData
+from ..data.utils import B1Calculator, SARCalculator
+
+import numpy as np
+import torch
+
+
+class TorchB1HomogeneityCost:
+    def __init__(self) -> None:
+        super().__init__()
+
+    def b1_calc(self, field):
+        b_field = field[1]
+        b_field_complex = b_field[0] + 1j * b_field[1]
+        return 0.5 * (b_field_complex[0] + 1j * b_field_complex[1])
+
+    def b1_homogeneity_cost(self, field):
+        b1_plus = self.b1_calc(field)
+        b1_plus_magnitude = torch.abs(b1_plus)
+        b1_plus_mean = torch.mean(b1_plus_magnitude)
+        b1_plus_std = torch.std(b1_plus_magnitude)
+        return b1_plus_mean / (b1_plus_std + 1e-6)
+
+    def calc_cost(self, field):
+        return -1 * self.b1_homogeneity_cost(field)
+
+
+class TorchB1HomogeneityCostSAR:
+    def __init__(self) -> None:
+        super().__init__()
+
+    def sars_calc(self, field, properties):
+        e_field = field[0]
+        abs_efield_sq = torch.sum(e_field**2, axis=(0, 1))
+
+        # get the conductivity and density tensors
+        conductivity = properties[0]
+        density = properties[2]
+
+        return conductivity * abs_efield_sq / density
+
+    def sar_cost(self, field, properties):
+        return torch.max(self.sars_calc(field, properties))
+
+    def calc_cost(self, field, prop, lambda_param=0.01):
+        b1_homogeneity_cost = TorchB1HomogeneityCost()
+        return -1 * b1_homogeneity_cost.b1_homogeneity_cost(
+            field
+        ) + lambda_param * self.sar_cost(field, prop)
---
+++ src/optimizers/torch_optimizer.py
@@ -0,0 +1,82 @@
+from ..data.simulation import Simulation, SimulationData, CoilConfig
+from skimage.measure import label, regionprops
+
+# from ..costs.base import BaseCost
+# from typing import Callable
+from einops import rearrange, repeat, einsum
+import numpy as np
+import torch
+import tqdm
+
+from tqdm import trange
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class PhaseShift(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.phase = torch.nn.Parameter(torch.randn(8, dtype=torch.float32))
+        self.amplitude = torch.nn.Parameter(torch.randn(8, dtype=torch.float32))
+
+    def forward(self, field):
+        re_phase = torch.cos(self.phase) * self.amplitude
+        im_phase = torch.sin(self.phase) * self.amplitude
+        coeffs_real = torch.stack((re_phase, -im_phase), dim=0)
+        coeffs_im = torch.stack((im_phase, re_phase), dim=0)
+        coeffs = torch.stack((coeffs_real, coeffs_im), dim=0)
+        coeffs = repeat(coeffs, "reimout reim coils -> hf reimout reim coils", hf=2)
+        field_shift = einsum(
+            field,
+            coeffs,
+            "hf reim fieldxyz ... coils, hf reimout reim coils -> hf reimout fieldxyz ...",
+        )
+        return field_shift
+
+
+class TorchOptimizer:
+
+    def __init__(self, cost_function, max_iter: int = 1000) -> None:
+        self.cost_fuction = cost_function
+        self.max_iter = max_iter
+
+    def field_and_prop(self, sim_data: Simulation):
+        labeled_mask = label(sim_data.simulation_raw_data.subject)
+        regions = regionprops(labeled_mask)
+        coords = regions[0].bbox
+        field_np = sim_data.simulation_raw_data.field[
+            :,
+            :,
+            :,
+            int(coords[0]) : int(coords[3]),
+            int(coords[1]) : int(coords[4]),
+            int(coords[2]) : int(coords[5]),
+            :,
+        ]
+        prop_np = sim_data.simulation_raw_data.properties[
+            :,
+            int(coords[0]) : int(coords[3]),
+            int(coords[1]) : int(coords[4]),
+            int(coords[2]) : int(coords[5]),
+        ]
+
+        field = torch.tensor(field_np, dtype=torch.float32).to(device)
+        prop = torch.tensor(prop_np, dtype=torch.float32).to(device)
+
+        return field, prop
+
+    def optimize(self, simulation: Simulation):
+        field, prop = self.field_and_prop(simulation)
+        model = PhaseShift().to(device)
+        optmizer = torch.optim.Adam(model.parameters(), lr=0.1)
+
+        pbar = trange(self.max_iter)
+        for i in pbar:
+            optmizer.zero_grad()
+            shifted_field = model(field)
+            loss = self.cost_fuction.calc_cost(shifted_field, prop)
+            loss.backward()
+            optmizer.step()
+
+            if i % 100 == 0:
+                print(f"Iteration {i}: Loss = {loss.item()}")
